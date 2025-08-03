412db2312bf1b9f043004e9a609907f0f78b6685
Jellytooooooo <taegu234@naver.com>
2025-08-04 03:20:20 +0900

    Add target lock gameplay ability and toggle input support
    Implemented a new Target Lock gameplay ability, including supporting C++ classes, input actions, and UI widget. Added toggleable input tag support and logic to the ability system, and updated gameplay tags to include target lock related tags. Also added a generic ability task for ticking logic and updated relevant assets for integration.

---
+++ Source/Warrior/Private/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.cpp
@@ -0,0 +1,155 @@
+
+#include "AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h"
+
+#include "WarriorFunctionLibrary.h"
+#include "WarriorGameplayTags.h"
+#include "Blueprint/WidgetLayoutLibrary.h"
+#include "Characters/WarriorHeroCharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Widgets/WarriorWidgetBase.h"
+#include "Blueprint/WidgetTree.h"
+#include "Components/SizeBox.h"
+#include "Controllers/WarriorHeroController.h"
+
+void UHeroGameplayAbility_TargetLock::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	TryLockOnTarget();
+
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+void UHeroGameplayAbility_TargetLock::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	CleanUp();
+
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UHeroGameplayAbility_TargetLock::OnTargetLockTick(float DeltaTime)
+{
+	const bool bIsLockedActorDead = UWarriorFunctionLibrary::NativeDoesActorHaveTag(CurrentLockedActor, WarriorGameplayTags::Shared_Status_Dead);
+	const bool bIsHeroCharacterDead = UWarriorFunctionLibrary::NativeDoesActorHaveTag(GetHeroCharacterFromActorInfo(), WarriorGameplayTags::Shared_Status_Dead);
+
+	if (!CurrentLockedActor || bIsLockedActorDead || bIsHeroCharacterDead)
+	{
+		CancelTargetLockAbility();
+		return;
+	}
+	SetTargetLockWidgetPosition();
+}
+
+void UHeroGameplayAbility_TargetLock::TryLockOnTarget()
+{
+	GetAvailableActorsToLock();
+
+	if (AvailableActorsToLock.IsEmpty())
+	{
+		CancelTargetLockAbility();
+		return;
+	}
+
+	CurrentLockedActor = GetNearestTargetFromAvailableActors(AvailableActorsToLock);
+
+	if (CurrentLockedActor)
+	{
+		DrawTargetLockWidget();
+		SetTargetLockWidgetPosition();
+	}
+	else
+	{
+		CancelTargetLockAbility();
+	}
+}
+
+void UHeroGameplayAbility_TargetLock::GetAvailableActorsToLock()
+{
+	TArray<FHitResult> BoxTraceHits;
+
+	UKismetSystemLibrary::BoxTraceMultiForObjects(
+		GetHeroCharacterFromActorInfo(),
+		GetHeroCharacterFromActorInfo()->GetActorLocation(),
+		GetHeroCharacterFromActorInfo()->GetActorLocation() + GetHeroCharacterFromActorInfo()->GetActorForwardVector() * TraceDistance,
+		TraceBoxSize / 2.f,
+		GetHeroCharacterFromActorInfo()->GetActorForwardVector().ToOrientationRotator(),
+		BoxTraceChannel,
+		false,
+		TArray<AActor*>(),
+		bShowPersistentDebugShape ? EDrawDebugTrace::Persistent : EDrawDebugTrace::None,
+		BoxTraceHits,
+		true);
+
+	for (const FHitResult& TraceHit : BoxTraceHits)
+	{
+		if (AActor* HitActor = TraceHit.GetActor())
+		{
+			if (HitActor != GetHeroCharacterFromActorInfo()) AvailableActorsToLock.AddUnique(HitActor);
+		}
+	}
+}
+
+AActor* UHeroGameplayAbility_TargetLock::GetNearestTargetFromAvailableActors(const TArray<AActor*>& InAvailableActors)
+{
+	float ClosestDistance = 0.f;
+	return UGameplayStatics::FindNearestActor(GetHeroCharacterFromActorInfo()->GetActorLocation(), InAvailableActors, ClosestDistance);
+}
+
+void UHeroGameplayAbility_TargetLock::DrawTargetLockWidget()
+{
+	if (!DrawnTargetLockWidget)
+	{
+		checkf(TargetLockWidgetClass, TEXT("Forgot to assign a valid widget class in Blueprint"));
+		DrawnTargetLockWidget = CreateWidget<UWarriorWidgetBase>(GetHeroControllerFromActorInfo(), TargetLockWidgetClass);
+		check(DrawnTargetLockWidget);
+		DrawnTargetLockWidget->AddToViewport();	
+	}
+}
+
+void UHeroGameplayAbility_TargetLock::SetTargetLockWidgetPosition()
+{
+	if (!DrawnTargetLockWidget || !CurrentLockedActor) 
+	{
+		CancelTargetLockAbility();
+		return;
+	}
+	FVector2D ScreenPosition;
+	UWidgetLayoutLibrary::ProjectWorldLocationToWidgetPosition(
+		GetHeroControllerFromActorInfo(), 
+		CurrentLockedActor->GetActorLocation(),
+		ScreenPosition,
+		true);
+
+	if (TargetLockWidgetSize == FVector2D::ZeroVector)
+	{
+		DrawnTargetLockWidget->WidgetTree->ForEachWidget(
+		[this](UWidget* FoundWidget)
+		{
+			if (const USizeBox* FoundSizeBox = Cast<USizeBox>(FoundWidget))
+			{
+				TargetLockWidgetSize.X = FoundSizeBox->GetWidthOverride();
+				TargetLockWidgetSize.Y = FoundSizeBox->GetHeightOverride();
+			}
+		});
+	}
+
+	ScreenPosition -= (TargetLockWidgetSize / 2.f);
+
+	DrawnTargetLockWidget->SetPositionInViewport(ScreenPosition, false);
+}
+
+void UHeroGameplayAbility_TargetLock::CancelTargetLockAbility()
+{
+	CancelAbility(GetCurrentAbilitySpecHandle(), GetCurrentActorInfo(), GetCurrentActivationInfo(), true);
+}
+
+void UHeroGameplayAbility_TargetLock::CleanUp()
+{
+	AvailableActorsToLock.Empty();
+
+	CurrentLockedActor = nullptr;
+
+	if (DrawnTargetLockWidget) DrawnTargetLockWidget->RemoveFromParent();
+
+	DrawnTargetLockWidget = nullptr;
+	TargetLockWidgetSize = FVector2D::ZeroVector;
+}
---
+++ Source/Warrior/Public/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/WarriorHeroGameplayAbility.h"
+#include "HeroGameplayAbility_TargetLock.generated.h"
+
+class UWarriorWidgetBase;
+
+UCLASS()
+class WARRIOR_API UHeroGameplayAbility_TargetLock : public UWarriorHeroGameplayAbility
+{
+	GENERATED_BODY()
+
+protected :
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+	UFUNCTION(BlueprintCallable)
+	void OnTargetLockTick(float DeltaTime);
+
+private :
+	void TryLockOnTarget();
+	void GetAvailableActorsToLock();
+	AActor* GetNearestTargetFromAvailableActors(const TArray<AActor*>& InAvailableActors);
+	void DrawTargetLockWidget();
+	void SetTargetLockWidgetPosition();
+
+	void CancelTargetLockAbility();
+	void CleanUp();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	float TraceDistance = 5000.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	FVector TraceBoxSize = FVector(5000.f, 5000.f, 300.f);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	TArray<TEnumAsByte<EObjectTypeQuery>> BoxTraceChannel;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	bool bShowPersistentDebugShape = false;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	TSubclassOf<UWarriorWidgetBase> TargetLockWidgetClass;
+
+	UPROPERTY()
+	TArray<AActor*> AvailableActorsToLock;
+
+	UPROPERTY()
+	AActor* CurrentLockedActor;
+
+	UPROPERTY()
+	TObjectPtr<UWarriorWidgetBase> DrawnTargetLockWidget;
+
+	UPROPERTY()
+	FVector2D TargetLockWidgetSize = FVector2D::ZeroVector;
+};
