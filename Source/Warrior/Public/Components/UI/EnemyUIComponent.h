4f500e4b9d0109744dac08710797a9ad20ff29d6
Jellytooooooo <taegu234@naver.com>
2025-08-06 04:49:10 +0900

    Add Frost Giant enemy and boss health bar UI
    Introduces the Frost Giant enemy with associated assets, AI, animation blueprints, abilities, and effects. Adds a new boss health bar UI and related materials. Updates EnemyUIComponent to manage enemy-drawn widgets. Also updates gameplay tags and engine config for new features.

--- Source/Warrior/Public/Components/UI/EnemyUIComponent.h
+++ Source/Warrior/Public/Components/UI/EnemyUIComponent.h
@@ -4,9 +4,22 @@
 #include "Components/UI/PawnUIComponent.h"
 #include "EnemyUIComponent.generated.h"
 
+class UWarriorWidgetBase;
+
 UCLASS()
 class WARRIOR_API UEnemyUIComponent : public UPawnUIComponent
 {
 	GENERATED_BODY()
+
+public :
+	UFUNCTION(BlueprintCallable)
+	void RegisterEnemyDrawnWidget(UWarriorWidgetBase* InWidgetToRegister);
+
+	UFUNCTION(BlueprintCallable)
+	void RemoveEnemyDrawnWidgetsIfAny();
+
+private :
+	UPROPERTY()
+	TArray<UWarriorWidgetBase*> EnemyDrawnWidgets;
 	
 };
